d8041407de151443a22b40a77e424500e58f8477
Fred K. Schott <fkschott@gmail.com>
2021-09-02 16:55:11 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,72 @@
-import { Client, Intents, MessageEmbed } from "discord.js";
-import { getConfig } from "./config/config";
-import { getRequiredPermissions } from "./helpers/permissionHelper";
+import { Client, Intents, MessageEmbed } from 'discord.js';
+import { getConfig } from './config/config';
+import { getRequiredPermissions } from './helpers/permissionHelper';
 
 const CLIENT = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const CONFIG = getConfig();
 
-CLIENT.once("ready", () => {
-	console.log("Ready!");
+CLIENT.once('ready', () => {
+  console.log('Ready!');
 });
 
-CLIENT.on("messageCreate", async message => {
-	// Server outage
-	if (!message.guild?.available) return;
+CLIENT.on('messageCreate', async (message) => {
+  // Server outage
+  if (!message.guild?.available) return;
 
-	// Not logged in
-	if (CLIENT.user === null) return;
+  // Not logged in
+  if (CLIENT.user === null) return;
 
-	const authorUser = message.author;
-	const authorMember = message.member;
-	const guild = message.guild;
-	const channel = message.channel;
+  const authorUser = message.author;
+  const authorMember = message.member;
+  const guild = message.guild;
+  const channel = message.channel;
 
-	if (authorUser.bot) return;
-	if (!channel.isText()) return;
-	if (!CONFIG?.threadChannels?.includes(channel.id)) return;
+  if (authorUser.bot) return;
+  if (!channel.isText()) return;
+  if (!CONFIG?.threadChannels?.includes(channel.id)) return;
 
-	const botMember = await guild.members.fetch(CLIENT.user);
-	const botPermissions = botMember.permissionsIn(message.channel.id);
-	const requiredPermissions = getRequiredPermissions();
-	if (!botPermissions.has(requiredPermissions)) {
-		try {
-			const missing = botPermissions.missing(requiredPermissions);
-			const errorMessage = `Missing permission${missing.length > 1 ? "s" : ""}:`;
-			await message.channel.send(`${errorMessage}\n    - ${missing.join("\n    - ")}`);
-		}
-		catch (e) {
-			console.log(e);
-		}
-		return;
-	}
+  const botMember = await guild.members.fetch(CLIENT.user);
+  const botPermissions = botMember.permissionsIn(message.channel.id);
+  const requiredPermissions = getRequiredPermissions();
+  if (!botPermissions.has(requiredPermissions)) {
+    try {
+      const missing = botPermissions.missing(requiredPermissions);
+      const errorMessage = `Missing permission${missing.length > 1 ? 's' : ''}:`;
+      await message.channel.send(`${errorMessage}\n    - ${missing.join('\n    - ')}`);
+    } catch (e) {
+      console.log(e);
+    }
+    return;
+  }
 
-	const creationDate = message.createdAt.toISOString().slice(0, 10);
-	const authorName = authorMember === null || authorMember.nickname === null
-		? authorUser.username
-		: authorMember.nickname;
+  const creationDate = message.createdAt.toISOString().slice(0, 10);
+  const authorName = authorMember === null || authorMember.nickname === null ? authorUser.username : authorMember.nickname;
 
-	const thread = await message.startThread({
-		name: `${authorName} (${creationDate})`,
-		autoArchiveDuration: CONFIG.threadArchiveDurationInMinutes,
-	});
+  const thread = await message.startThread({
+    name: `${authorName.replace(/\(.*/, '').trim()} (${creationDate})`,
+    autoArchiveDuration: CONFIG.threadArchiveDurationInMinutes,
+  });
 
-	const embed = new MessageEmbed()
-		.setAuthor(authorName, authorUser.displayAvatarURL())
-		.setDescription(message.content)
-		.setColor([255, 94, 0]);
+  const channelMention = `<#${channel.id}>`;
+  const teamMention = channel.id === '872579324446928896' ? `<@&857704834597650472>` : `<@&857704834597650472> <@&882699029706862602>`;
+  const relativeTimestamp = `<t:${Math.round(message.createdTimestamp / 1000)}:R>`;
 
-	const channelMention = `<#${channel.id}>`;
-	const relativeTimestamp = `<t:${Math.round(message.createdTimestamp / 1000)}:R>`;
+  const threadMsg = await thread.send({
+    embeds: [], // [embed],
+    content: `Hey <@${authorUser.id}>! I've automatically created this helpful thread from your message ${relativeTimestamp}.\n\nPinging ${teamMention} so that they see this as well!`,
+  });
 
-	const threadMsg = await thread.send({
-		embeds: [embed],
-		content: `Thread created from ${channelMention} by <@${authorUser.id}> ${relativeTimestamp} with the following message:`,
-	});
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
 
-	await threadMsg.pin();
-	await thread.leave();
+  const embed = new MessageEmbed().setAuthor(authorName, authorUser.displayAvatarURL()).setDescription(`Thanks, @user! :taco: :taco: :taco:`).setColor([0, 206, 201]);
+
+  // const tacosMention = await thread.send({
+  // embeds: [embed],
+  // content: `*Hint: was someone extra helpful in this thread? Say thank you with tacos!*`,
+  // });
+
+  // await threadMsg.pin();
+  await thread.leave();
 });
 
 CLIENT.login(CONFIG.discordApiToken);
